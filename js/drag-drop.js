a9870d923a609ff570f453d15456d2b60f5e13ec
Verta-master <vertyagina@gmail.com>
2017-12-20 11:22:01 +0600

    File upload

--- js/drag-drop.js
+++ js/drag-drop.js
@@ -1,13 +1,60 @@
 'use strict';
 
-// работа с формой
+// Загрузка картинок
 (function () {
-  var dropZones = document.querySelectorAll('.drop-zone');
-  var avatarDropZone = dropZones[0];
-  var imageDropZone = dropZones[1];
+  var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
+
+  var avatarDropZone = document.querySelector('.notice__photo .drop-zone');
+  var imageDropZone = document.querySelector('.form__photo-container .drop-zone');
   var avatarImage = document.querySelector('.notice__preview img');
   var uploadImageArea = document.querySelector('.form__photo-container');
+  var avatarFile = document.querySelector('#avatar');
+  var imageFile = document.querySelector('#images');
+  imageFile.setAttribute('multiple', true);
 
+  // Загрузка через input[type=file]
+  avatarFile.addEventListener('change', function () {
+    var file = avatarFile.files[0];
+    var fileName = file.name.toLowerCase();
+
+    var matches = FILE_TYPES.some(function (item) {
+      return fileName.endsWith(item);
+    });
+
+    if (matches) {
+      var reader = new FileReader();
+
+      reader.addEventListener('load', function () {
+        avatarImage.src = reader.result;
+      });
+      reader.readAsDataURL(file);
+    }
+  });
+
+  imageFile.addEventListener('change', function () {
+    for (var i = 0; i < imageFile.files.length; i++) {
+      var file = imageFile.files[i];
+      var fileNames = file.name.toLowerCase();
+
+      var matches = FILE_TYPES.some(function (item) {
+        return fileNames.endsWith(item);
+      });
+
+      if (matches) {
+        var reader = new FileReader();
+
+        reader.addEventListener('load', function () {
+          var img = document.createElement('IMG');
+          img.width = '40';
+          uploadImageArea.appendChild(img);
+          img.src = reader.result;
+        });
+        reader.readAsDataURL(file);
+      }
+    }
+  });
+
+  // Загрузка drag-and-drop
   avatarDropZone.addEventListener('dragenter', function (evt) {
     evt.stopPropagation();
     evt.preventDefault();
@@ -28,7 +75,6 @@
     reader.addEventListener('load', function () {
       avatarImage.src = reader.result;
     });
-
     reader.readAsDataURL(file);
   });
 
@@ -47,25 +93,17 @@
     evt.stopPropagation();
     evt.preventDefault();
     evt.dataTransfer.dropEffect = 'copy';
-    var file = evt.dataTransfer.files[0];
-    var reader = new FileReader();
+    for (var j = 0; j < evt.dataTransfer.files.length; j++) {
+      var file = evt.dataTransfer.files[j];
+      var reader = new FileReader();
 
-    reader.addEventListener('load', function () {
-      var img = document.createElement('IMG');
-      img.width = '100';
-      uploadImageArea.appendChild(img);
-      img.src = reader.result;
-      img.setAttribute('draggable', true);
-    });
-
-    reader.readAsDataURL(file);
-  });
-
-  imageDropZone.addEventListener('dragstart', function (evt) {
-    if (evt.target.tagName.toLowerCase() === 'img') {
-      // var draggedItem = evt.target;
-      evt.dataTransfer.setData('text/plain', evt.target.alt);
+      reader.addEventListener('load', function () {
+        var img = document.createElement('IMG');
+        img.width = '40';
+        uploadImageArea.appendChild(img);
+        img.src = reader.result;
+      });
+      reader.readAsDataURL(file);
     }
   });
-
 })();
